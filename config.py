603c2e36eb82ae2f688062f6a9b371e5fc2f162b
UnknownGuestUser <therockherogaming@gmail.com>
2026-03-25 15:58:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/UnknownGuestUser/AarohixMusic",
+    "https://github.com/UnknownGuestUser/ShrutiMusicDatabase",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
